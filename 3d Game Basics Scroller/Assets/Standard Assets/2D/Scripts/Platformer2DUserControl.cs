bf8e2fcf3922380ced37478b176cfb76f65ccfc8
jeremie.mortier <jeremie.mortier@epitech.eu>
2017-11-08 17:43:42 +0100

    Some mechanics have been added Some animations have been done Maybe we shouldn't put a wall jump?

---
+++ 3d Game Basics Scroller/Assets/Standard Assets/2D/Scripts/Platformer2DUserControl.cs
@@ -0,0 +1,60 @@
+using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Platformer2DUserControl : MonoBehaviour
+{
+	public Rigidbody2D player;
+	public float speed;
+	public Animator	anim;
+	private float gravity = 0.4f;
+	private float vg = 0;
+	private bool  allowJump = false;
+	private int test = 0;
+	private bool facingRight = true;
+	private float h = 0;
+
+    private void Awake()
+    {
+		player = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();
+    }
+
+
+    private void Update()
+    {
+		h = CrossPlatformInputManager.GetAxis("Horizontal");
+		Flip ();
+		if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow)) {
+			anim.SetInteger ("State", 1);
+		} else {
+			anim.SetInteger ("State", 0);
+		}
+		float jump = CrossPlatformInputManager.GetAxis ("Jump");
+		if (jump != 0 && allowJump) {
+			vg = 10;
+			allowJump = false;
+		}
+		if (!allowJump) {
+			anim.SetInteger ("State", 2);
+		}
+		vg -= gravity;
+		player.velocity = new Vector2 (h * speed, vg);
+    }
+
+	private void Flip()
+	{
+		if ((h > 0f && !facingRight) || (h < 0f && facingRight)) {
+			facingRight = !facingRight;
+			Vector3 tmp = transform.localScale;
+			tmp.x *= -1;
+			transform.localScale = tmp;
+		}
+	}
+
+	void OnCollisionStay2D(Collision2D coll)
+	{
+		vg = 0;
+		allowJump = true;
+	}
+}
---
+++ 3d Game Basics Scroller/Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
@@ -0,0 +1,15 @@
+using System;
+using UnityEngine;
+
+namespace UnityStandardAssets._2D
+{
+    public class PlatformerCharacter2D : MonoBehaviour
+    {
+        [SerializeField] private float m_MaxSpeed = 10f;
+
+        public void Move()
+        {
+           
+        }
+    }
+}
