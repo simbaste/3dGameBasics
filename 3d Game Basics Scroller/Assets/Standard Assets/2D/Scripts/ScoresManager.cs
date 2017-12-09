7a0d5892c84f709145f46f4356ad13adef785d54
simomb_s <stephanedarcy.simomba@epitech.eu>
2017-12-10 00:26:15 +0900

    Update Menu Score

--- 3d Game Basics Scroller/Assets/Standard Assets/2D/Scripts/ScoresManager.cs
+++ 3d Game Basics Scroller/Assets/Standard Assets/2D/Scripts/ScoresManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using System;
 using System.Data;
 using System.IO;
@@ -11,13 +12,16 @@ public class ScoresManager : MonoBehaviour {
 	private String connectionString;
 
 	private List<Score> Scores = new List<Score>();
+	Transform scorePanel;
 
 	public GameObject scorePrefab;
 	public Transform scoreParent;
+	public int saveScores;
 
 	// Use this for initialization
 	void Start () {
 
+		scorePanel = transform.Find ("ScoreMenu");
 		connectionString = "URI=file:"+Application.dataPath+"/DB/3DGameScoreDB";
 		if (!File.Exists (Application.dataPath+"/DB/3DGameScoreDB")) {
 			using (IDbConnection dbConnection = new SqliteConnection (connectionString)) {
@@ -64,18 +68,55 @@ public class ScoresManager : MonoBehaviour {
 		Scores.Sort ();
 	}
 
-	public void InsertScore(int newScore) {
-		using (IDbConnection dbConnection = new SqliteConnection(connectionString)) {
-			dbConnection.Open ();
+	public void LimitScoreSize() {
+		scorePanel.GetChild (4).gameObject.SetActive (true);
+	}
 
-			using (IDbCommand dbCmd = dbConnection.CreateCommand()) {
-				string sqlQuery = string.Format("INSERT INTO Scores(Score) VALUES({0})", newScore);
-				Debug.Log ("sqlQuery = " + sqlQuery);
-				sqlQuery = "INSERT INTO Scores(Score) VALUES("+newScore+")";		
-				Debug.Log ("sqlQuery = " + sqlQuery);
-				dbCmd.CommandText = sqlQuery;
-				dbCmd.ExecuteScalar ();
-				dbConnection.Close ();
+	public void SetLimitScore() {
+		int scoreLimit;
+
+		if (int.TryParse(scorePanel.GetChild (4).GetComponentInChildren<InputField>().text, out scoreLimit)) {
+			if (scoreLimit > 0) {
+				saveScores = scoreLimit;
+				DeleteExtraScore ();
+			}
+		}
+
+		foreach (RectTransform child in scorePanel.GetChild(2).GetChild(0)) {
+			Destroy ((child as Transform).gameObject);
+		}
+
+		scorePanel.GetChild (4).gameObject.SetActive (false);
+		ShowScores ();
+	}
+
+	public void InsertScore(int newScore) {
+
+		GetScore ();
+		int hsCount = Scores.Count;
+
+		if (Scores.Count > 0) {
+			Score lowestScore = Scores[Scores.Count - 1];
+
+			if (lowestScore != null && saveScores > 0 && Scores.Count >= saveScores && newScore > lowestScore.score) {
+				deleteScore (lowestScore.ID);
+				hsCount--;
+			}
+		}
+
+		if (hsCount < saveScores) {
+			using (IDbConnection dbConnection = new SqliteConnection(connectionString)) {
+				dbConnection.Open ();
+
+				using (IDbCommand dbCmd = dbConnection.CreateCommand()) {
+					string sqlQuery = string.Format("INSERT INTO Scores(Score) VALUES({0})", newScore);
+					Debug.Log ("sqlQuery = " + sqlQuery);
+					sqlQuery = "INSERT INTO Scores(Score) VALUES("+newScore+")";		
+					Debug.Log ("sqlQuery = " + sqlQuery);
+					dbCmd.CommandText = sqlQuery;
+					dbCmd.ExecuteScalar ();
+					dbConnection.Close ();
+				}
 			}
 		}
 	}
@@ -88,6 +129,46 @@ public class ScoresManager : MonoBehaviour {
 			tmpObject.GetComponent<ScoreScript> ().SetScore ("#"+(i+1).ToString(), tmpScore.score.ToString(), tmpScore.Date.ToString());
 			tmpObject.transform.SetParent (scoreParent);
 			tmpObject.GetComponent<RectTransform> ().localScale = new Vector3 (1, 1, 1);
+		}
+	}
+
+	private void deleteScore(int id) {
+		GetScore ();
+
+		using (IDbConnection dbConnection = new SqliteConnection (connectionString)) {
+			dbConnection.Open ();
+
+			using(IDbCommand dbCommand = dbConnection.CreateCommand()) {
+
+				string sqlQuery = String.Format ("DELETE FROM Scores WHERE ScoreID = \"{0}\"", id);
+
+				dbCommand.CommandText = sqlQuery;
+				dbCommand.ExecuteScalar ();
+				dbConnection.Close ();
+			}
+		}
+	}
+
+	private void DeleteExtraScore() {
+		GetScore ();
+		if (saveScores <= Scores.Count) {
+			int deleteCount = Scores.Count - saveScores;
+			Scores.Reverse ();
+
+			using (IDbConnection dbConnection = new SqliteConnection (connectionString)) {
+				dbConnection.Open ();
+
+				using(IDbCommand dbCommand = dbConnection.CreateCommand()) {
+
+					for (int i = 0; i < deleteCount; i++) {
+						string sqlQuery = String.Format ("DELETE FROM Scores WHERE ScoreID = \"{0}\"", Scores[i].ID);
+
+						dbCommand.CommandText = sqlQuery;
+						dbCommand.ExecuteScalar ();
+					}
+					dbConnection.Close ();
+				}
+			}
 		}
 	}
 }
